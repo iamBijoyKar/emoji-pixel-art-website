e8638bbf0ec74aea6b8478aa33cf24cd7781d1e5
iamBijoyKar <bijoypoco12345@gmail.com>
2023-02-17 16:47:10 +0530

    menu button added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@
 function FooterLinks(){
 
     return(
-        <div className="flex flex-col md:flex-row lg:inline-flex">
+        <div className="flex flex-col md:flex-row lg:inline-flex gap-4">
             <a className="" href="https://github.com/iamBijoyKar/emojis-pixel-art/blob/master/CONTRIBUTING.md">Contribute</a>
             <a className="" href="/">About</a>
             <a className="" href="https://github.com/iamBijoyKar/emojis-pixel-art/blob/master/LICENSE">License</a>
@@ -16,7 +16,7 @@ export default function Footer(){
 
 
     return(
-        <footer className="bg-grey flex flex-row justify-between align-middle ">
+        <footer className="bg-grey p-8 flex flex-row justify-between align-middle ">
 
             <div className="ml-8">
                 Copyright © Bijoy Kar 2023
